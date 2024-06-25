2d73f1db4763d9413c95515351be6ad12b63c307
Aryan Sharma <128162387+Aryan741x@users.noreply.github.com>
2024-06-26 01:35:07 +0530

    Create Modular_Exponentiation.cpp

---
+++ Extra/Modular_Exponentiation.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+long long mod_exp(long long a,long long b,long long M){
+    a%=M;
+    long long r=1;
+    while(b>0){
+        if(b&1){
+            r=(r*a)%M;
+        }
+        else{
+            a=(a*a)%M;
+        }
+        b/=2;
+    }
+    return r;
+}
+int main(){
+    long long a=20;
+    long long b=4;
+    long long M=1e9+7;
+    long long result=mod_exp(a,b,M);
+    cout<<result<<endl;
+}
