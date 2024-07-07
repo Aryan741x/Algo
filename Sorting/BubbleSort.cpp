62009b961c7073a01a8ccaf4f0327799acc5597c
Aryan Sharma <128162387+Aryan741x@users.noreply.github.com>
2024-07-08 02:06:21 +0530

    Update BubbleSort.cpp

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -1,7 +1,12 @@
 void bubbleSort(int arr[], int n) {
     for(int i=0;i<n;++i){
+        bool r=false;
         for(int j=0;j<n-i-1;++j){
-            if(arr[j]>arr[j+1])swap(arr[j],arr[j+1]);
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                r=true;
+            }
         }
+        if(!r)break;
     }
 }
